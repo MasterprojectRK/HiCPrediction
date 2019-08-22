0fd792aee2f188f0ac99fa36d34e207a570b7acb
Ubuntu <ab@bajnet.de>
2019-08-22 15:35:00 +0000

    setup file

---
+++ HiCPrediction/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hiCPrediction-abajorat",
+    version="0.0.1",
+    author="Andre Bajorat",
+    author_email="abajorat@posteo.de",
+    description="Package to predict HiC-matrices based on proteins",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abajorat/MasterProjekt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['HiCPrediction/createBaseFile.py', 'HiCPrediction/predict',
+             'HiCPrediction/training.py', 'HiCPrediction/createTrainingSet.py',
+            'HiCPrediction/plotMatrix.py'],
+     install_requires=[
+          'joblib', 'intervaltree', 'multiprocess', 'pandas', 'future',
+         'scikit-learn', 'unidecode','matplotlib', 'pyarrow', 'cooler', 
+     'pybedtools', 'pyBigWig', 'hicmatrix', 'hicexplorer'],
+)
