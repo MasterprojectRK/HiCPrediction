f23db1538f3e4e25c189bd6998f1114ac55f9cad
ralf <nobody@email.sucks>
2019-12-05 14:40:29 +0100

    add input checking for regions. check if compare matrix has the same chromosome.

--- hicprediction/scripts/plotMatrix.py
+++ hicprediction/scripts/plotMatrix.py
@@ -17,8 +17,8 @@ import matplotlib.cm as cm
 import click
 from matplotlib.colors import LogNorm
 
-@click.option('--regionIndex1', '-r1',default=1, show_default=True, required=True)
-@click.option('--regionIndex2','-r2', default=1000, show_default=True, required=True)
+@click.option('--regionIndex1', '-r1',default=1, show_default=True, required=True, type=click.IntRange(min=0) )
+@click.option('--regionIndex2','-r2', default=1000, show_default=True, required=True, type=click.IntRange(min=0))
 @click.option('--matrixinputfile', '-mif',type=click.Path(exists=True), required=True)
 @click.option('--imageoutputfile','-iof', default=None)
 @click.option('--comparematrix', '-cmp', type=click.Path(exists=True))
@@ -29,15 +29,26 @@ def plotMatrix(matrixinputfile,imageoutputfile, regionindex1, regionindex2, comp
         if not imageoutputfile:
             imageoutputfile = matrixinputfile.rstrip('cool') + 'png'
         conf.checkExtension(imageoutputfile, 'png')
-            
+       
         #get the full matrix first to extract the desired region
         ma = hm.hiCMatrix(matrixinputfile)
         cuts = ma.cut_intervals
         chromosome = cuts[0][0]
+        maxIndex = len(cuts) - 1
+        #check indices and get the region if ok
+        if regionindex1 > maxIndex:
+            msg = "invalid start region. Allowed is 0 to {0:d} (0 to {1:d})".format(maxIndex, cuts[maxIndex][1])
+            sys.exit(msg)
+        if regionindex2 < regionindex1:
+           msg = "region index 2 must be smaller than region index 1"
+           sys.exit(msg)
+        if regionindex2 > maxIndex:
+            regionindex2 = maxIndex
+            print("region index 2 clamped to max. value {0:d}".format(maxIndex))
         region = str(chromosome) +":"+str(cuts[regionindex1][1])+"-"+ str(cuts[regionindex2][1])
         
         #now get the data for the input matrix, restricted to the desired region
-        upperHiCMatrix = hm.hiCMatrix(matrixinputfile ,pChrnameList=[region]) #todo: load region from matrix
+        upperHiCMatrix = hm.hiCMatrix(matrixinputfile ,pChrnameList=[region])
         upperMatrix = triu(upperHiCMatrix.matrix, k=1, format="csr")
         
         #if set, get data from the same region also for the compare matrix
@@ -45,11 +56,15 @@ def plotMatrix(matrixinputfile,imageoutputfile, regionindex1, regionindex2, comp
         lowerHiCMatrix = None
         lowerMatrix = None
         if comparematrix:
+            lowerHiCMatrix = hm.hiCMatrix(comparematrix)
+            if chromosome not in [row[0] for row in lowerHiCMatrix.cut_intervals]:
+                msg = "compare matrix must contain the same chromosome as the input matrix"
+                sys.exit(msg)
             lowerHiCMatrix = hm.hiCMatrix(comparematrix , pChrnameList=[region])
             lowerMatrix = tril(lowerHiCMatrix.matrix, k=0, format="csr") 
-        
+
             if lowerMatrix.get_shape() != upperMatrix.get_shape():
-                msg = "shapes of input matrix and compare matrix do not match"
+                msg = "shapes of input matrix and compare matrix do not match. Check resolutions"
                 sys.exit(msg)
 
         #arguments for plotting
@@ -135,82 +150,6 @@ def plotMatrix(matrixinputfile,imageoutputfile, regionindex1, regionindex2, comp
         #cooler throws bad input error
         ##lowerHiCMatrix.matrix = lowerMatrix + upperMatrix
         ##lowerHiCMatrix.save("test.cool", pSymmetric=False)
-
-# def plotMatrix(args):
-    # for i in range(1,4):
-        # print(i)
-        # args.regionIndex1 = i*500 + 1
-        # args.regionIndex2 = (i+1)*500
-        # name = args.sourceFile.split(".")[0].split("/")[-1]
-        # a = ["--matrix",args.sourceFile,
-                # "--dpi", "300"]
-        # if args.log:
-            # a.extend(["--log1p", "--vMin" ,"1","--vMax" ,"1000"])
-        # else:
-            # a.extend(["--vMax" ,"1","--vMin" ,"0"])
-        # if args.region:
-            # a.extend(["--region", args.region])
-            # name = name + "_r"+args.region
-        # elif args.regionIndex1 and args.regionIndex2:
-            # ma = hm.hiCMatrix(args.sourceFile)
-            # cuts = ma.cut_intervals
-            # region = args.chrom +":"+str(cuts[args.regionIndex1][1])+"-"+ str(cuts[args.regionIndex2][1])
-            # a.extend(["--region", region])
-            # name = name + "_R"+region
-
-        # a.extend( ["-out", IMAGE_D+name+".png"])
-        # hicPlot.main(a)
-
-
-# def plotDir(args):
-    # for cs in [11,14,17,9,19]:
-        # args.chroms = str(cs)
-        # for c in ["9_A"]:
-            # args.chrom = str(c)
-            # for p in ["default"]:
-                # args.conversion = p
-                # for w in ["avg"]:
-                    # args.windowOperation = w
-                    # for me in ["avg"]:
-                        # args.mergeOperation = me
-                        # for m in ["rf"]:
-                            # args.model = m
-                            # for n in [False]:
-                                # args.normalizeProteins = n
-                                # for n in [False]:
-                                    # args.equalizeProteins = n
-                                    # if os.path.isfile(tagCreator(args,"pred")):
-                                        # for i in range(5):
-                                            # args.regionIndex1 = i*500 + 1
-                                            # args.regionIndex2 = (i+1)*500
-                                            # plotPredMatrix(args)
-
-
-# def concatResults():
-    # sets = []
-    # for a in os.listdir(RESULTPART_D):
-        # if a.split("_")[0] == "part":
-            # if os.path.isfile(RESULTPART_D + a):
-                # sets.append(pickle.load(open(RESULTPART_D + a, "rb" ) ))
-                # print(len(pickle.load(open(RESULTPART_D + a, "rb" ) )))
-    # sets.append(pickle.load(open(RESULT_D+"baseResults.p", "rb" ) ))
-    # df_all = pd.concat(sets)
-    # df_all = df_all.drop_duplicates()
-    # print(len(df_all))
-    # # df_all = df_all[~df_all.index.duplicated()]
-    # print(df_all[df_all.index.duplicated()])
-    # print(len(df_all))
-
-    # pickle.dump(df_all, open(RESULT_D+"baseResults.p", "wb" ) )
-
-# def mergeAndSave():
-    # d = RESULT_D
-    # now = str(datetime.datetime.now())[:19]
-    # now = now.replace(":","_").replace(" ", "")
-    # src_dir = d + "baseResults.p"
-    # dst_dir = d + "/Old/old"+str(now)+".p"
-    # shutil.copy(src_dir,dst_dir)
-    # concatResults()
 
 if __name__ == '__main__':
     plotMatrix()
