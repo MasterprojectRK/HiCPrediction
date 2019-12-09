a8be0835543be8bf594e3e19417f2e94fb4e44b0
ralf <nobody@email.sucks>
2019-12-09 13:18:21 +0100

    allow compound datasets

--- hicprediction/tagCreator.py
+++ hicprediction/tagCreator.py
@@ -1,16 +1,28 @@
+import numpy as np
+
 def createSetTag(params):
-    tmp = params['cellType'] + '_' + params['resolution']
+    #compound sets may comprise multiple cell types, window operations and chromosomes
+    cellTypeList = list( np.hstack([], params['cellType']) )
+    windowOpList = list( np.hstack([], params['windowOperation']) )
+    chromList = list( np.hstack([], params['chrom']) )
+    cellTypeStr = "_".join(cellTypeList)
+    windowOpStr = "_".join(windowOpList)
+    chromStr = "_".join(chromList)
+    tmp = cellTypeStr + '_' + params['resolution']
     tmp +='_'+ createProteinTag(params)
-    tmp += '_W' + params['windowOperation']
+    tmp += '_W' + windowOpStr
     tmp += str(params['windowSize'])
 
     if params['ignoreCentromeres']:
         tmp += '_A'
-    return tmp + params['chrom']
+    return tmp + chromStr
 
 def createProteinTag(params):
+    #compound sets may comprise multiple merge operations
+    mergeOpList = list( np.hstack([], params['mergeOperation']) )
+    mergeOpStr = "_".join(mergeOpList)
     tmp = ''
-    tmp += 'M' +params['mergeOperation']
+    tmp += 'M' + mergeOpStr
     if params['normalize']:
         tmp += '_N'
     if params['peakColumn']  != 6:
