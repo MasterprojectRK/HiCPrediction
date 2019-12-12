cf6ec482882fcdc840a8f404de775f2fdcea808d
ralf <nobody@email.sucks>
2019-12-12 02:29:30 +0100

    remove uncessary file

--- configurations.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp hicprediction/configurations.py  /home/ralf/miniconda3/envs/hicpred/lib/python3.6/site-packages/hicprediction/configurations.py
-cp hicprediction/tagCreator.py  /home/ralf/miniconda3/envs/hicpred/lib/python3.6/site-packages/hicprediction/tagCreator.py
