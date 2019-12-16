1d1e6c9772fd0b8b56d9817c65ef7ad157c3babf
ralf <nobody@email.sucks>
2019-12-16 13:22:26 +0100

    ignore existing models for now

--- hicprediction/training.py
+++ hicprediction/training.py
@@ -37,7 +37,9 @@ def training(modeloutputdirectory, conversion, traindatasetfile):
     params['conversion'] = conversion
     modelTag = createModelTag(params) + ".z"
     modelFileName = os.path.join(modeloutputdirectory, modelTag)
-    if not os.path.isfile(modelFileName):
+    #exists = os.path.isfile(modelFileName)
+    exists = False
+    if not exists:
         ### create model with desired parameters
         model = sklearn.ensemble.RandomForestRegressor(max_features='sqrt',random_state=5,\
                     n_estimators =10,n_jobs=4, verbose=2, criterion='mse')
