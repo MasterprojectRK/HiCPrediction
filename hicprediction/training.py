36b8f8ea164a2dc01c07ccbc26d65d3526de1fc8
ralf <nobody@email.sucks>
2020-01-15 14:56:51 +0100

    allow training both on multiColumn and oneHot Datasets, if they contain reads

--- hicprediction/training.py
+++ hicprediction/training.py
@@ -36,44 +36,68 @@ def training(modeloutputdirectory, conversion, pNrOfTrees, pMaxFeat, traindatase
         sys.exit(msg.format(traindatasetfile))
     ### load data set and set parameters
     df, params = joblib.load(traindatasetfile)
+    ### check if the dataset contains reads, otherwise it cannot be used for training
+    nanreadMask = df['reads'] == np.nan
+    if not df[nanreadMask].empty:
+        #any nans coming from bad bins in the HiC-matrix must be replaced when creating the dataset
+        #so there should not be any nans left
+        msg = "dataset {0:s} cannot be used for training" 
+        msg += "because it contains no target reads"
+        raise ValueError(msg.format(traindatasetfile))
+    
+    ### store the training parameters for later reference and file name creation
     params['conversion'] = conversion
     params['noDistance'] = noDist
     params['noMiddle'] = noMiddle
     params['noStartEnd'] = noStartEnd
+        
+    ### create model with desired parameters
+    model = sklearn.ensemble.RandomForestRegressor(max_features=pMaxFeat, random_state=5,\
+                    n_estimators=pNrOfTrees, n_jobs=4, verbose=2, criterion='mse')
+    
+    ### replace infinity and nan values. There shouldn't be any.
+    rowsBefore = df.shape[0]
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df.fillna(value=0, inplace=True)
+
+    ### drop columns that should not be used for training
+    dropList = ['first', 'second', 'chrom', 'reads', 'avgRead']
+    if noDist:
+        dropList.append('distance')    
+    if noMiddle:
+        if params['method'] == 'oneHot':
+            dropList.append('middleProt')
+        elif params['method'] == 'multiColumn':
+            numberOfProteins = int((df.shape[1] - 6) / 3)
+            for protein in range(numberOfProteins):
+                dropList.append(str(protein + numberOfProteins))
+        else:
+            raise NotImplementedError()
+    if noStartEnd:
+        if params['method'] == 'oneHot':
+            dropList.append('startProt')
+            dropList.append('endProt')
+        elif params['method'] == 'multiColumn':
+            numberOfProteins = int((df.shape[1] - 6) / 3)
+            for protein in range(numberOfProteins):
+                dropList.append(str(protein))
+                dropList.append(str(protein + 2 * numberOfProteins))
+        else:
+            raise NotImplementedError()
+    X = df[df.columns.difference(dropList)]
+        
+    ### apply conversion
+    if conversion == 'none':
+        y = df['reads']
+    elif conversion == 'standardLog':
+        y = np.log(df['reads']+1)
+
+    ## train model and store it
+    model.fit(X, y)
     modelTag = createModelTag(params) + ".z"
     modelFileName = os.path.join(modeloutputdirectory, modelTag)
-    #exists = os.path.isfile(modelFileName)
-    exists = False
-    if not exists:
-        ### create model with desired parameters
-        model = sklearn.ensemble.RandomForestRegressor(max_features=pMaxFeat, random_state=5,\
-                    n_estimators=pNrOfTrees, n_jobs=4, verbose=2, criterion='mse')
-        df.replace([np.inf, -np.inf], np.nan, inplace=True)
-        df.fillna(value=0, inplace=True)
-        ### drop columns that should not be used for training
-        dropList = ['first', 'second', 'chrom', 'reads', 'avgRead']
-        if noDist:
-            dropList.append('distance')
-        if noMiddle:
-            dropList.append('middleProt')
-        if noStartEnd:
-            dropList.append('startProt')
-            dropList.append('endProt')    
-        X = df[df.columns.difference(dropList)]
-        
-        ### apply conversion
-        if conversion == 'none':
-            y = df['reads']
-        elif conversion == 'standardLog':
-            y = np.log(df['reads']+1)
-
-        ## train model and store it
-        model.fit(X, y)
-        joblib.dump((model, params), modelFileName, compress=True ) 
-        print("\n")
-    else:
-
-        print("Skipped creating model that already existed: " + modelFileName)
+    joblib.dump((model, params), modelFileName, compress=True ) 
+    print("\n")
 
 if __name__ == '__main__':
     train()
