b7873f2669b3000b487ecfefff7c9caa104e8b19
ralf <nobody@email.sucks>
2020-02-12 16:23:39 +0100

    only 4 jobs to alleviate memory problems

--- hicprediction/training.py
+++ hicprediction/training.py
@@ -325,7 +325,7 @@ def visualizeModel(pTreeBasedLearningModel, pXtrain, pYtrain, pOutDir, pFeatList
 
     #see https://scikit-learn.org/stable/auto_examples/inspection/plot_permutation_importance.html#sphx-glr-auto-examples-inspection-plot-permutation-importance-py
     result = permutation_importance(pTreeBasedLearningModel, pXtrain, pYtrain, n_repeats=1,
-                                random_state=42, n_jobs=-1)
+                                random_state=42, n_jobs=4)
 
     fig1, ax1 = plt.subplots()
     ax1.boxplot(result.importances.T, vert=True)
