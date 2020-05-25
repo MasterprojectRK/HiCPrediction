f307c4a07c1dcd22a6e41027d982584c4ea3f174
ralf <nobody@email.sucks>
2020-05-25 20:30:25 +0200

    installation improved

--- setup.py
+++ setup.py
@@ -2,45 +2,40 @@ from setuptools import setup, find_packages
 import versioneer
 
 requirements = [
-            'bedtools>=2.29.2',
-            'click>=7.0',
-            'cooler>=0.8.7',
-            'graphviz>=2.42.3',
-            'h5py>=2.10.0',
-            'hicexplorer>=3.4.1',
-            'hicmatrix>=11',
-            'hyperopt>=0.2.4',
-            'joblib>=0.14.1',
-            'matplotlib>=3.1.3'
-            'numpy>=1.18.1' 
-            'pandas>=1.0.1',
-            'pybedtools>=0.8.0',
-            'pybigwig>=0.3.17',
-            'pydot>=1.4.1'
-            'scikit-learn>=0.22.1',
-            'scipy>=1.4.1'
-            'tqdm>=4.43.0',    
+            'click',
+            'cooler',
+            'graphviz',
+            'h5py',
+            'hicmatrix',
+            'hyperopt',
+            'joblib',
+            'matplotlib',
+            'numpy', 
+            'pandas',
+            'pybedtools',
+            'pyBigWig >=0.3.*',
+            'pydot',
+            'scikit-learn',
+            'scipy',
+            'tqdm',    
 ]
 
 setup(
-    name='HiCPrediction',
-    version="0.0.2",
+    name='hicprediction',
+    version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description="HiCMatrix Prediction via protein levels",
     author="Andre Bajorat, Ralf Krauth",
     url='https://github.com/MasterprojectRK/HiCPrediction',
-    packages=find_packages(where='hicprediction'),
+    packages=find_packages(),
+    include_package_data = True,
     package_data={'hicprediction': ['hicprediction/scripts/*.py']},
-
-    scripts=[
-             'hicprediction/createBaseFile.py',
-             'hicprediction/predict.py',
-             'hicprediction/training.py',
-             'hicprediction/createTrainingSet.py',
-            ],
     install_requires=requirements,
+    entry_points = {
+        'console_scripts': ['createBaseFile=hicprediction.createBaseFile:loadAllProteins',
+                            'createTrainingSet=hicprediction.createTrainingSet:createTrainingSet',
+                            'training=hicprediction.training:train',
+                            'prediction=hicprediction.predict:executePredictionWrapper']
+    },
     keywords='HiCPrediction',
-    classifiers=[
-        'Programming Language :: Python :: 3.6',
-    ],
 )
