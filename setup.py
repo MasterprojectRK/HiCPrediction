63f1794640406b0205e8143075bb157dcc85fa55
ralf <nobody@email.sucks>
2020-05-25 17:35:25 +0200

    trying to get working setup

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
 
 requirements = [
-            'joblib', 
-            'pandas',
-            'future',
-            'scikit-learn',
-            'unidecode',
-            'matplotlib',
-            'pyarrow',  
+            'bedtools>=2.29.2',
+            'click>=7.0',
+            'cooler>=0.8.7',
+            'graphviz>=2.42.3',
+            'h5py>=2.10.0',
+            'hicexplorer>=3.4.1',
+            'hicmatrix>=11',
+            'hyperopt>=0.2.4',
+            'joblib>=0.14.1',
+            'matplotlib>=3.1.3'
+            'numpy>=1.18.1' 
+            'pandas>=1.0.1',
+            'pybedtools>=0.8.0',
+            'pybigwig>=0.3.17',
+            'pydot>=1.4.1'
+            'scikit-learn>=0.22.1',
+            'scipy>=1.4.1'
+            'tqdm>=4.43.0',    
 ]
 
 setup(
     name='HiCPrediction',
-    version="0.0.1",
+    version="0.0.2",
     cmdclass=versioneer.get_cmdclass(),
     description="HiCMatrix Prediction via protein levels",
-    author="Andre Bajorat",
-    author_email='abajorat@posteo.de',
-    url='https://github.com/abajorat/HiCPrediction',
-    packages=['hicprediction'],
-    package_data={'hicprediction': ['InternalStorage/centromeres.txt']},
-    # },
+    author="Andre Bajorat, Ralf Krauth",
+    url='https://github.com/MasterprojectRK/HiCPrediction',
+    packages=find_packages(where='hicprediction'),
+    package_data={'hicprediction': ['hicprediction/scripts/*.py']},
+
     scripts=[
              'hicprediction/createBaseFile.py',
              'hicprediction/predict.py',
